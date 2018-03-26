c28dd618d55cc1a78b8a5b3c38f6f70250d19661
Brandon Alejandro Mosqueda González <brandonMG_1698@hotmail.com>
2018-03-26 11:43:10 -0600

    get and post, with params in the url inserted in the forms views with jade

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ var express = require("express");
 
 var app = express();
 
+//to work the view's render
 app.set("view engine", "jade");
 
 //the root site "/"
 app.get("/", function(req, res){
-	res.render("index", {hola: "brandon"});
+	res.render("index");
+})
+
+app.get("/:nombre", function(req, res){
+	var nombre = req.params.nombre;
+	console.log(nombre);
+	res.render("form", {name: nombre});
+})
+
+app.post("/", function(req, res) {
+	res.render("form");
 })
 
 app.listen(8080);
